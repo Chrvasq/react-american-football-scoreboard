a4959cecb7e1eb251801ba8e05538cf4f35bf9ab
Chris Vasquez <chrvasq@outlook.com>
2019-09-19 17:51:34 -0700

    Fix away team name

--- src/Away.js
+++ src/Away.js
@@ -4,7 +4,7 @@ import "./App.css";
 const Away = props => {
   return (
     <div className="away">
-      <h2 className="away__name">Lions</h2>
+      <h2 className="away__name">Tigers</h2>
       <div className="away__score">{props.score.awayScore}</div>
     </div>
   );
